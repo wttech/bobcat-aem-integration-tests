cb471fbb0ef1907be0f5a74e2bf4235ad1e66fce
Mikołaj Mański <mmanski@users.noreply.github.com>
2017-03-09 14:51:07 +0100

    Update updateBobcatVersion.sh

--- updateBobcatVersion.sh
+++ updateBobcatVersion.sh
@@ -10,6 +10,7 @@ if [ "$(echo $?)" -eq 0 ]; then
 	echo "Integration tests are using the latest version of Bobcat. No changes required";
 else
 	echo "Stale version of Bobcat dependency detected in the repository! Commiting changes with the latest versions in project's pom file"
+	git checkout master
 	git add pom.xml
 	git commit -m "Updated Bobcat to the latest available version"
 	git push -q
